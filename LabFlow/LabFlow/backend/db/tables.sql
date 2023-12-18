6babca888f6559492c89ef4b6d803a6c94458223
Chas Ladhar <137926064+chas-lad@users.noreply.github.com>
2023-12-18 09:28:24 +0000

    Added scrolling and pinch zooming in and out features

--- LabFlow/LabFlow/backend/db/tables.sql
+++ LabFlow/LabFlow/backend/db/tables.sql
@@ -4,8 +4,67 @@ CREATE TABLE users (
     surname   VARCHAR(50),
     email     VARCHAR(50),
     userName  VARCHAR(50),
-    pass      VARCHAR(72), -- max character limit that bcrypt will create hashes to
+    pass      VARCHAR(72), -- SHA 256 is 64 characters max
     staff     BIT DEFAULT 0
 );
 
-DELETE FROM users;
+INSERT INTO users
+(firstName, surname, email, userName, pass, staff)
+VALUES
+("John", "Doe", "johndoe@gmail.com", "johndoe", "$2b$1", 0),
+("Jane", "Smith", "janesmith@gmail.com", "janesmith", "$2b$1", 0),
+("Michael", "Johnson", "michaeljohnson@gmail.com", "michaeljohnson", "$2b$1", 0),
+("Emily", "Williams", "emilywilliams@gmail.com", "emilywilliams", "$2b$1", 0),
+("Daniel", "Brown", "danielbrown@gmail.com", "danielbrown", "$2b$1", 0),
+("Olivia", "Jones", "oliviajones@gmail.com", "oliviajones", "$2b$1", 0),
+("Matthew", "Taylor", "matthewtaylor@gmail.com", "matthewtaylor", "$2b$1", 0),
+("Sophia", "Miller", "sophiamiller@gmail.com", "sophiamiller", "$2b$1", 0),
+("Andrew", "Anderson", "andrewanderson@gmail.com", "andrewanderson", "$2b$1", 0),
+("Emma", "Thomas", "emmathomas@gmail.com", "emmathomas", "$2b$1", 0),
+("William", "Jackson", "williamjackson@gmail.com", "williamjackson", "$2b$1", 0),
+("Ava", "White", "avawhite@gmail.com", "avawhite", "$2b$1", 0),
+("James", "Harris", "jamesharris@gmail.com", "jamesharris", "$2b$1", 0),
+("Mia", "Martin", "miamartin@gmail.com", "miamartin", "$2b$1", 0),
+("Benjamin", "Clark", "benjaminclark@gmail.com", "benjaminclark", "$2b$1", 0),
+("Charlotte", "Lewis", "charlottelewis@gmail.com", "charlottelewis", "$2b$1", 0),
+("Joseph", "Lee", "josephlee@gmail.com", "josephlee", "$2b$1", 0),
+("Chloe", "Walker", "chloewalker@gmail.com", "chloewalker", "$2b$1", 0),
+("David", "Hall", "davidhall@gmail.com", "davidhall", "$2b$1", 0),
+("Grace", "Young", "graceyoung@gmail.com", "graceyoung", "$2b$1", 0);
+
+CREATE TABLE machines (
+    id           INT IDENTITY(1,1) PRIMARY KEY,
+    xPos         INT,
+    yPos         INT,
+    labID        INT FOREIGN KEY REFERENCES labs(id),
+    commonIssues VARCHAR(1000),
+    user_id      INT FOREIGN KEY REFERENCES users(id)
+);
+
+CREATE TABLE labs (
+    id                  INT IDENTITY(1,1) PRIMARY KEY,
+    locationDescription VARCHAR(1000),
+    labName             VARCHAR(50),
+    wheelchairAccess    BIT DEFAULT 1
+);
+
+CREATE TABLE friends (
+    friend1       INT FOREIGN KEY REFERENCES users(id),
+    friend2       INT FOREIGN KEY REFERENCES users(id),
+    friendStatus  INT DEFAULT 0
+);
+
+CREATE TABLE teachingSchedule (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    module VARCHAR(10),
+    lecturerName VARCHAR(50),
+    dayOfWeek VARCHAR(10),
+    startTime TIME,
+    endTime TIME,
+    labId INT FOREIGN KEY REFERENCES labs(id)
+);
+
+INSERT INTO machines
+(xPos, yPos, labID, commonIssues, user_id)
+VALUES
+(0, 0, 1, "One monitor doesn't work, often doesn't work with logins", 1);
--- LabFlow/LabFlow/components/Home.js
+++ LabFlow/LabFlow/components/Home.js
@@ -1,61 +1,97 @@
-import React from 'react';
-import { View, Text, FlatList, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { ScrollView, PinchGestureHandler, State } from 'react-native-gesture-handler';
 
 function Home({ navigation }) {
+  const [zoomLevel, setZoomLevel] = useState(1);
 
   const machineData = [
-    { id: 1, name: 'Machine 1', position: { x: 0, y: 0 } },
-    { id: 2, name: 'Machine 2', position: { x: 1, y: 0 } },
-    { id: 3, name: 'Machine 3', position: { x: 2, y: 0 } },
-    { id: 4, name: 'Machine 4', position: { x: 0, y: 1 } },
-    { id: 5, name: 'Machine 5', position: { x: 1, y: 1 } },
-    { id: 6, name: 'Machine 6', position: { x: 2, y: 1 } },
-    { id: 7, name: 'Machine 7', position: { x: 3, y: 2 } },
-    { id: 8, name: 'Machine 8', position: { x: 4, y: 2 } },
-    { id: 9, name: 'Machine 9', position: { x: 5, y: 2 } },
-    { id: 10, name: 'Machine 10', position: { x: 6, y: 10 } },
-    // Add more machines with their positions
+    { name: 'Machine 1', position: { x: 0, y: 0 } },
+    { name: 'Machine 2', position: { x: 1, y: 0 } },
+    { name: 'Machine 3', position: { x: 2, y: 0 } },
+    { name: 'Machine 4', position: { x: 3, y: 0 } },
+    { name: 'Machine 5', position: { x: 4, y: 8 } },
+    { name: 'Machine 6', position: { x: 7, y: 18 } },
+    { name: 'Machine 7', position: { x: 24, y: 15 } },
   ];
 
-  // size of 1 container
-  const containerSize = 100; // Adjust based on your design
+  const containerSize = 100;
+  const sensitivity = 0.2; // Adjust the zoom sensitivity as needed
+  const minZoomLevel = 0.5; // Adjust the minimum zoom level
+  const maxZoomLevel = 2; // Adjust the maximum zoom level
+  const padding = 50; // Adjust the padding as needed
 
-  // Render item component for FlatList
-  const renderItem = ({ item }) => (
-    <TouchableOpacity onPress={() => handleMachinePress(item)}>
-      <View style={{
-        padding: 10,
-        margin: 5,
-        borderWidth: 1,
-        borderColor: 'black',
-        position: 'absolute',
-        left: item.position.x * containerSize,
-        top: item.position.y * containerSize,
-        width: containerSize,
-        height: containerSize,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        <Text>{item.name}</Text>
-      </View>
-    </TouchableOpacity>
-  );
+
+
+  const handleMachinePress = (item) => {
+    // Handle machine press event
+  };
+
+  const handlePinch = (event) => {
+    const scale = event.nativeEvent.scale;
+
+    // Adjust the zoom level within the specified range with sensitivity
+    setZoomLevel((prevZoom) => {
+      const newZoom = prevZoom * (1 + sensitivity * (scale - 1));
+      return Math.min(Math.max(newZoom, minZoomLevel), maxZoomLevel);
+    });
+  };
+
+  const calculateContentSize = () => {
+    const maxX = Math.max(...machineData.map((item) => item.position.x)) + 1;
+    const maxY = Math.max(...machineData.map((item) => item.position.y)) + 1;
+
+    const contentWidth = maxX * containerSize * zoomLevel + padding * 2;
+    const contentHeight = maxY * containerSize * zoomLevel + padding * 2;
+
+    return { contentWidth, contentHeight };
+  };
+
+  const { contentWidth, contentHeight } = calculateContentSize();
+
+  const renderItem = ({ item }) => {
+    const adjustedSize = containerSize * zoomLevel;
+    const adjustedX = item.position.x * adjustedSize + padding;
+    const adjustedY = item.position.y * adjustedSize + padding;
+
+    return (
+      <TouchableOpacity onPress={() => handleMachinePress(item)}>
+        <View
+          style={{
+            padding: 10,
+            margin: 5,
+            borderWidth: 1,
+            borderColor: 'black',
+            position: 'absolute',
+            left: adjustedX,
+            top: adjustedY,
+            width: adjustedSize,
+            height: adjustedSize,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text>{item.name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
 
   return (
-<ScrollView
-  horizontal
-  vertical
-  style={{ flex: 1 }}
-  contentContainerStyle={{
-    flexGrow: 1,
-    // dynamically calculate width based on the number of machines
-    width: machineData.length * containerSize,
-    // dynamically calculate height based on the maximum y position
-    height: Math.max(...machineData.map(item => item.position.y + 1)) * containerSize,
-  }}
->
-  {machineData.map((item) => renderItem({ item }))}
-</ScrollView>
+    <PinchGestureHandler onGestureEvent={handlePinch}>
+      <ScrollView
+        horizontal
+        vertical
+        style={{ flex: 1 }}
+        contentContainerStyle={{
+          flexGrow: 1,
+          width: contentWidth,
+          height: contentHeight,
+        }}
+      >
+        {machineData.map((item) => renderItem({ item }))}
+      </ScrollView>
+    </PinchGestureHandler>
   );
 }
 
