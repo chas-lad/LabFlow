a662e1aa4aaa4dca58fdf5965bcbac92e24fff21
Chas Ladhar <137926064+chas-lad@users.noreply.github.com>
2023-12-19 20:05:13 +0000

    Add users API endpoint and implement friend search functionality

--- LabFlow/LabFlow/backend/functions/users/__init__.py
+++ LabFlow/LabFlow/backend/functions/users/__init__.py
@@ -1,44 +1,38 @@
-import azure.functions as func
-from db import db_connector
 import logging
+from db import db_connector
+import azure.functions as func
 import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    if req.method == 'POST':
+    if req.method == 'GET':
         try:
             connection = db_connector()
             cursor = connection.cursor()
-
-            req_body = req.get_json()
-
-            labID = req_body.get('labID')
 
             cursor.execute("""
                            SELECT
                                 *
                            FROM 
-                                machines
-                           WHERE
-                                labID = ?
-                           """, (labID))
+                                users
+                           """)
                            
-            machines = cursor.fetchall()
+            users = cursor.fetchall()
 
              # Convert the result to a list of dictionaries
-            machines_list = [dict(zip([column[0] for column in cursor.description], row)) for row in machines]
+            users_list = [dict(zip([column[0] for column in cursor.description], row)) for row in users]
 
             cursor.close()
             connection.close()
 
             # Return the result as JSON
-            return func.HttpResponse(json.dumps(machines_list), mimetype="application/json", status_code=200)
+            return func.HttpResponse(json.dumps(users_list), mimetype="application/json", status_code=200)
         except Exception as e:
             logging.error(f"Error: {str(e)}")
-            return func.HttpResponse("Failed to retrieve machines data", status_code=500)
+            return func.HttpResponse("Failed to retrieve users data", status_code=500)
     else:
         return func.HttpResponse(
-            'Invalid request method. Use POST.',
+            'Invalid request method. Use GET.',
             status_code=400
         )
--- LabFlow/LabFlow/components/Friends.js
+++ LabFlow/LabFlow/components/Friends.js
@@ -1,19 +1,57 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
 
 const Friends = () => {
     const [friendName, setFriendName] = useState('');
-    const [matchedFriends, setMatchedFriends] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [matchedUsers, setMatchedUsers] = useState([]);
 
+    // Fetch all users from database when comoonent mounts for the first time
+    // and store them in the users array. We don't want to fetch the users
+    // each time the user types a letter in the input field.
+    useEffect(() => {
+    const fetchUsersFromDatabase = async () => {
+        try {
+          const response = await fetch(
+            'https://labflowbackend.azurewebsites.net/api/users?',
+            {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            }
+          );
+  
+          const fetchedUsers = await response.json();
+          setUsers(fetchedUsers);
+  
+        } catch (error) {
+          console.error('Error fetching users:', error);
+        }
+      };
+  
+      fetchUsersFromDatabase();
+    }, [])
+    
     const handleFriendNameChange = (text) => {
         setFriendName(text);
-        // Perform matching logic here to filter friends based on user input
-        // const matched = friends.filter((friend) =>
-        //     friend.toLowerCase().includes(text.toLowerCase())
-        // );
-        // setMatchedFriends(matched);
+        // Perform logic to find friends that match the text inputted so far by
+        // using the firstName and surName fields of the users in the users array
+        const matchedUsers = [];
+        users.forEach((user) => {
+            // If the user's first name or surname or entire name contains the text inputted so far,
+            // add the user to the matchedUsers array
+            if (user.firstName.toLowerCase().includes(text.toLowerCase()) ||
+                user.surname.toLowerCase().includes(text.toLowerCase())   ||
+                (user.firstName + ' ' + user.surname).toLowerCase().includes(text.toLowerCase())) {
+                matchedUsers.push(user);
+            }
+        });
+
+        setMatchedUsers(matchedUsers);
     };
 
+    console.log(matchedUsers)
     const handleAddFriend = () => {
         // Perform logic to add friend to your data structure
         // Reset the input field and matched friends list
@@ -22,21 +60,55 @@ const Friends = () => {
     };
 
     return (
-        <View>
-            <Text>Friends Component</Text>
-            <TextInput
-                value={friendName}
-                onChangeText={handleFriendNameChange}
-                placeholder="Type a friend's name"
-            />
-            <Button title="Add" onPress={handleAddFriend} />
-            <FlatList
-                data={matchedFriends}
-                renderItem={({ item }) => <Text>{item}</Text>}
-                keyExtractor={(item) => item}
-            />
+        <View style={styles.container}>
+          <Text style={styles.headerText}>Enter a user to add as a friend</Text>
+          <TextInput
+            value={friendName}
+            onChangeText={handleFriendNameChange}
+            placeholder="Type a friend's name"
+            style={styles.input}
+          />
+          <FlatList
+            data={friendName && matchedUsers.map((user) => user.firstName + ' ' + user.surname)}
+            renderItem={({ item }) => <Text style={styles.matchedUserText}>{item}</Text>}
+            keyExtractor={(item) => item.id}
+            style={styles.flatList}
+          />
         </View>
-    );
-};
-
-export default Friends;
+      );
+    };
+    
+    const styles = StyleSheet.create({
+      container: {
+        padding: 20,
+        backgroundColor: 'white',
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: 'gray',
+      },
+      headerText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+      },
+      input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        padding: 10,
+      },
+      flatList: {
+        backgroundColor: 'white',
+        marginTop: 10,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+      },
+      matchedUserText: {
+        fontSize: 18,
+        marginBottom: 15,
+      },
+    });
+    
+    export default Friends;
